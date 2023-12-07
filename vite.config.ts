95aa8c85d9c7a7d1c35ef0d40b1d6a29211e8e86
Sopi Septiansyah <sopi.septiansyah@efishery.com>
2023-12-07 11:27:04 +0700

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './sopiseptiansyah.github.io/',
+  base: '/sopiseptiansyah-gh-pages/',
   server: {
     port: 3000,
     watch: {
